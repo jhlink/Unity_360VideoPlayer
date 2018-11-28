18d17c3d29f83a16ab15a642e36dd4082c167639
jhlink <jhlink@users.noreply.github.com>
2018-11-27 22:38:40 -0500

    feat: Create AssetContainer class for local file storage management

---
+++ Assets/Scripts/AssetContainer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+using System.IO;
+
+public class AssetContainer
+{
+
+	private string mAssetHttpEndpoint = "";
+	private string mAssetAssignedFileName = "fileBlob.txt";
+	private string mAssetLocalFilePath = "";
+
+	public AssetContainer ()
+	{
+	}
+
+	public AssetContainer (String assetHttpEndpoint, String assetFileName)
+	{
+		mAssetHttpEndpoint = assetHttpEndpoint;
+		mAssetAssignedFileName = assetFileName;
+	}
+
+	public string AssetHttpEndpoint { 
+		get {
+			return mAssetHttpEndpoint;
+		}
+
+		set {
+			mAssetHttpEndpoint = value;
+		}
+	}
+
+	public string AssignedAssetFiledName { 
+		get {
+			return mAssetAssignedFileName;
+		} 
+		set {
+			mAssetAssignedFileName = value; 
+		}
+	}
+
+	public string AssetLocalFilePath {
+		get {
+			return mAssetLocalFilePath;
+		}
+		set {
+			if (!String.IsNullOrEmpty (value)) {
+				mAssetLocalFilePath = value;
+			}
+		}
+	}
+
+	public void deleteFileFromSystem ()
+	{
+		if (doesFileExistLocally ()) {
+			File.Delete (Application.persistentDataPath + mAssetAssignedFileName);
+		  Debug.Log("Delete: File deleted");	
+			// Is this necessary? ... We'll check it eventually.
+			Application.Quit ();
+		} else {
+		  Debug.Log("Delete: Unneeded - File does not exist");	
+		}
+	}
+
+	public bool doesFileExistLocally() {
+		bool result = File.Exists (Application.persistentDataPath + mAssetAssignedFileName);
+		String fileCheckString = " - Exists Locally: " + result;
+    String verifyFileExists = "Verify: File " + mAssetAssignedFileName + fileCheckString;
+
+		Debug.Log (verifyFileExists);
+
+		return result;
+	}
+}
