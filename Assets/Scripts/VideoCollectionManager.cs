54098097449137d3f4d1e87ecd938a55acbb121e
jhlink <jhlink@users.noreply.github.com>
2018-11-29 14:52:41 -0500

    feat: Create VideoCollectionManager class

---
+++ Assets/Scripts/VideoCollectionManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.Assertions;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoCollectionManager : MonoBehaviour
+{
+	public string[] videoID;
+	public string[] videoUrls;
+
+	private Dictionary<string, string> videoStringMap;
+
+	private void Start() {
+		assertVideoArrayConditions ();
+	}
+
+	private void assertVideoArrayConditions() {
+		Assert.raiseExceptions = true;
+		Assert.AreEqual(videoUrls.Length, videoID.Length);
+	}
+
+	private void initializeDictionary() {
+		if (videoStringMap.Count > 0) {
+			return;
+		}
+
+		if (videoID.Length == videoUrls.Length) {
+			for (int i = 0; i < videoID.Length; i++) {
+				string videoIdItem = videoID[ i ];
+				string videoUrlItem = videoUrls[ i ];
+
+				videoStringMap.Add (videoIdItem, videoUrlItem);
+
+				Debug.Log("Manager: " + videoIdItem + " at URL " + videoStringMap[videoIdItem] );
+			}
+		}
+	}
+}
+
